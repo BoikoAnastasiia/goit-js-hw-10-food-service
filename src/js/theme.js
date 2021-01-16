7c5962d345bfdcc03bc7152d7f5a8e46698d192d
anastasyah86 <nastasyah86@gmail.com>
2021-01-16 20:28:11 +0200

    theme toggler doesn't properly work

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,21 +3,27 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-localStorage.setItem('theme', JSON.stringify(Theme));
-const value = localStorage.getItem('theme');
-// console.log(value);
+// localStorage.setItem('theme', JSON.stringify(Theme));
+const savedSettings = localStorage.getItem('theme');
+// const settedTheme = JSON.parse(savedSettings);
+// console.log(settedTheme);
 
 const refs = {
   checkbox: document.querySelector('#theme-switch-toggle'),
   body: document.querySelector('body'),
 };
-console.dir(refs.checkbox);
+// console.dir(refs.checkbox);
 
 refs.checkbox.addEventListener('input', toggler);
 
 function toggler() {
   if (refs.checkbox.checked === true) {
     refs.body.classList.add('dark-theme');
+  } else if (
+    refs.checkbox.checked === false ||
+    elem.classList.contains('dark-theme')
+  ) {
+    elem.classList.replace('dark-theme', 'light-theme');
   }
-  refs.body.classList.add('light-theme');
 }
+//   refs.body.classList.add('light-theme');
