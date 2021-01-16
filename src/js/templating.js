225b09398327be3d03b89a811408e94c56344864
anastasyah86 <nastasyah86@gmail.com>
2021-01-16 16:56:14 +0200

    layout almost done, map left

---
+++ src/js/templating.js
@@ -0,0 +1,10 @@
+import itemsTemp from '../templates.hbs';
+import dishes from '../menu.json';
+import '../styles.scss';
+console.log(dishes);
+
+const markup = itemsTemp(dishes);
+console.log(markup);
+
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', markup);
