9c328390d406376348e8b40fa2a0aeb3320a6315
anastasyah86 <nastasyah86@gmail.com>
2021-01-16 18:06:38 +0200

    done

--- src/js/templating.js
+++ src/js/templating.js
@@ -8,8 +8,3 @@ console.log(markup);
 
 const galleryRef = document.querySelector('.js-menu');
 galleryRef.insertAdjacentHTML('beforeend', markup);
-
-Handlebars.registerHelper('list', function (items, options) {
-  const itemsAsHtml = items.map(item => '<li>' + options.fn(item) + '</li>');
-  return '<ul>\n' + itemsAsHtml.join('\n') + '\n</ul>';
-});
