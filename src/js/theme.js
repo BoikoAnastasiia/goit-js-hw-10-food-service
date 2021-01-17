d05ad2910d90ffb7a6bab469b7450787d71a8c57
anastasyah86 <nastasyah86@gmail.com>
2021-01-17 21:04:28 +0200

    local storage theme done

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,28 +3,28 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-// const savedSettings = localStorage.getItem('theme');
-// const settedTheme = JSON.parse(savedSettings);
-// console.log(settedTheme);
-
 const refs = {
   checkbox: document.querySelector('#theme-switch-toggle'),
   body: document.querySelector('body'),
+  theme: localStorage.getItem('theme'),
 };
-// console.dir(refs.checkbox);
 
 refs.checkbox.addEventListener('change', toggler);
 
 function toggler() {
   refs.body.classList.add('dark-theme');
+  localStorage.setItem('theme', 'dark-theme');
 }
 
 function changeThemeIf() {
   if (!refs.checkbox.checked && refs.body.classList.contains('dark-theme')) {
     refs.body.classList.replace('dark-theme', 'light-theme');
+    localStorage.setItem('theme', 'light-theme');
   }
 }
 
 refs.checkbox.addEventListener('change', changeThemeIf);
 
-localStorage.setItem('theme', JSON.stringify(Theme));
+if (refs.theme === 'dark-theme') {
+  toggler();
+}
